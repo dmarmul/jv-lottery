25b0abb3caa38ee11d0b2b5bf0f13c3fdf433943
Dmytro <156954070+dmarmul@users.noreply.github.com>
2024-02-20 20:57:10 +0200

    Add files via upload

---
+++ src/main/java/core/basesyntax/Application.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Application {
+    public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+
+        Ball[] balls = new Ball[3];
+        for (Ball ball: balls){
+            ball = lottery.getRandomBall();
+            System.out.println(ball);
+        }
+    }
+}
